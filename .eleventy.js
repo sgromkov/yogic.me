5b3f463126ed574bc58f48df0aaf063a465615b1
Sergey Gromkov <sgromkov@gmail.com>
2021-01-16 21:17:54 +0300

    add hash to assets

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const getDateInRu = require("./src/scripts/utilities/getDateInRU");
@@ -12,6 +13,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(cacheBuster({}));
 
   eleventyConfig.setDataDeepMerge(true);
 
